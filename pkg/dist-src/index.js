c578afd826ad3f4a412731401cc6fa9e6889e9c0
Tim McMaster <tim@mcmaster.id.au>
2019-11-26 16:40:12 +1100

    debugging build process

---
+++ pkg/dist-src/index.js
@@ -0,0 +1,4 @@
+import './vaadin-grid/vaadin-grid.js';
+import './vaadin-text-field/vaadin-text-field.js';
+import './vaadin-checkbox/vaadin-checkbox.js';
+import './vaadin-button/vaadin-button.js';
